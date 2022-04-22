2855e2a1355413da3c6f5b01e4ed440eb64fbf3a
luckoff <neluckoff@gmail.com>
2022-04-22 17:17:30 +0300

    added the ability to start a queue in a conversation

--- commands/admin_commands.py
+++ commands/admin_commands.py
@@ -61,7 +61,7 @@ async def shaffle_q(message: Message):
             await message.answer(queue_shaffled)
 
 
-@vk.on.private_message(text='Старт')
+@vk.on.private_message(text=['Старт', 'Начать очередь'])
 async def start_q(message: Message):
     if new_queue.is_empty():
         await message.answer(queue_not_created)
@@ -148,11 +148,11 @@ async def change_person(message: Message, args):
             if user[0].id in admin_list:
                 if args[0].isdigit():
                     if int(args[0]) <= len(new_queue.print_queue()):
-                        await vk.api.messages.send(peer_id=new_queue.get_by_id(int(args[0])-1).get_id(),
+                        await vk.api.messages.send(peer_id=new_queue.get_by_id(int(args[0]) - 1).get_id(),
                                                    message=f'{user[0].first_name} {user[0].last_name} убрал Вас из очереди!',
                                                    random_id=0)
                         await message.answer(f'Вы удалили {int(args[0])} пользователя!')
-                        new_queue.del_person(int(args[0])-1)
+                        new_queue.del_person(int(args[0]) - 1)
                     else:
                         await message.answer(f'Вы переборщили с числом, всего в списке '
                                              f'{len(new_queue.print_queue())} человек(-а).')
@@ -167,3 +167,25 @@ async def admin_help(message: Message):
     user = await vk.api.users.get(message.from_id)
     if user[0].id in admin_list:
         await message.answer(admin_help_str)
+
+
+@vk.on.message(text='/start')
+async def start_q(message: Message):
+    if new_queue.is_empty():
+        await message.answer(queue_not_created)
+    else:
+        user = await vk.api.users.get(message.from_id)
+        if user[0].id in admin_list:
+            users_array = id_users_csv()
+            array_q = new_queue.print_queue()
+            id = array_q[0].get_id()
+            await vk.api.messages.send(peer_ids=users_array, message="Очердь стартовала, первому игроку приготовиться.",
+                                       random_id=0)
+            await vk.api.messages.send(peer_id=id, message=your_next, random_id=0, keyboard=keyboard_answer)
+
+            new_str = ''
+            count = 1
+            for i in array_q:
+                new_str += f'{count}. {i}\n'
+                count += 1
+            await message.answer("Очередь была запущена, список:\n\n" + new_str)
