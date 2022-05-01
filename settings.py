11c3ba45a27f7213dcdf199d58808ea32d991ef3
Dmitry Luckyanenko <neluckoff@gmail.com>
2022-05-01 19:06:58 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,2 @@
-from pathlib import Path
-
-path = Path('data', 'csv', 'data.csv')
-
 """Переменная для хранения всех ID администрации"""
 admin_list = []  # int ids administration, example: 146653997
